9f5906a8c4fd42714fe4cb91361d99911a92dde6
Renoir Boulanger <hello@renoirboulanger.com>
2013-09-30 09:25:51 -0400

    Adding string sum examples, removing broken IP address to HEX sample

--- src/ZCE/StringsAndPatterns/Strings.php
+++ src/ZCE/StringsAndPatterns/Strings.php
@@ -27,27 +27,105 @@ class Strings
         '@'    => ' at ',
         '.'    => ' dot '
     );
+
+    static $IP      = '192.168.0.1';
+
+    public static function asciiRange($start, $stop)
+    {
+        $out = '';
+
+        if($stop > 256 || $stop < 0 || $start > 256 || $start < 0) {
+            throw new Exception('ASCII range is limited to 0..256');
+        }
+
+        for ($i = $start; $i <= $stop; ++$i) {
+          $out .= '    - chr('.$i.') will output "'.chr($i).'"'.PHP_EOL;
+        }
+
+        return $out;
+    }
+
+    public static function asciiValue($word)
+    {
+        $sum = 0;
+        $len = strlen($word);
+        die($len);
+        for($i = 0;$i <= $len; ++$i) {
+            $sum .= substr($word,$i);
+        }
+
+        return $sum;
+    }
+
+    /**
+     * [AsciiToInt description]
+     *
+     * Origin: http://php.net/manual/en/function.ord.php
+     *
+     * @param string $char An ASCII valid character
+     */
+    public static function AsciiToInt($word){
+        $success = "";
+        $len = strlen($word);
+        $sum = 0;
+        if($len == 1) {
+            return "char(".ord($word).")";
+        } elseif($len > 256) {
+            throw new Exception('Character "'.$word.'" is out of ASCII range');
+        } else {
+            for($i = 0; $i < $len; $i++){
+                if($i == $len - 1) {
+                    $success = $success.ord($word[$i]);
+                } else {
+                    $success = $success.ord($word[$i]).",";
+                }
+                $sum += ord($word[$i]);
+            }
+            return 'word "'.$word.'" has ASCII chars = {'.$success.'}, with sum: '.$sum;
+        }
+    }
 }
+
+
+echo PHP_EOL.'List ASCII character value and its position representation:'.PHP_EOL;
+echo PHP_EOL.'  48..57 represent in ASCII 0..9 numbers:'.PHP_EOL;
+echo Strings::asciiRange(48,57).PHP_EOL;
+echo PHP_EOL.'  65..90 represent in ASCII alphabet UPPER case:'.PHP_EOL;
+echo Strings::asciiRange(65,90).PHP_EOL;
+echo PHP_EOL.'  97..122 represent in ASCII alphabet in lower CASE:'.PHP_EOL;
+echo Strings::asciiRange(97,122).PHP_EOL;
+
+echo PHP_EOL.'Sum of each letter ASCII value of a word:'.PHP_EOL;
+echo '  - '.Strings::AsciiToInt('Hello').PHP_EOL;
+echo '  - '.Strings::AsciiToInt('hello').PHP_EOL;
+echo '  - '.Strings::AsciiToInt('Portugal').PHP_EOL;
+echo '  - '.Strings::AsciiToInt('Lisbon').PHP_EOL;
 
 /*
  *************************************
  * String Comparison
  *************************************
+ * Ref: http://php.net/manual/en/function.strcmp.php
+ *
+ * Remember that it is neither the length of the word, nor
+ * the difference. But the difference between the sum of
  */
 // strcmp function - case-sensitive
-echo PHP_EOL.'Using strcmp function - case-sensitive'.PHP_EOL;
-print_r(strcmp(Strings::$ONE, Strings::$FIVE)."\n");    // 'hello' vs "hello" = 0
-print_r(strcmp(Strings::$THREE, Strings::$FOUR)."\n");  // 'Portugal' vs 'Lisbon' = 4
-print_r(strcmp(Strings::$TWO, Strings::$FIVE)."\n");    // 'Hello' vs "hello" = -32
-print_r(strcmp(Strings::$TWO, Strings::$ONE)."\n");     // 'Hello' vs 'hello' = -32
-print_r(strcmp(Strings::$ONE, Strings::$TWO)."\n");     // 'hello' vs 'Hello' = 32
-print_r(strcmp('1', '0125')."\n");                      // '1' vs '0125' = 1
-print_r(strcmp('1', 1)."\n");                           // '1' vs 1 = 0
-print_r(strcmp('1', 01)."\n");                          // '1' vs 01 = 0
-print_r(strcmp('01', 01)."\n");                         // '01' vs 01 = -1
-print_r(strcmp('1', '01')."\n");                        // '1' vs '01' = 1
-print_r(strcmp('125', '25')."\n");                      // '125' vs '25' = -1
-print_r(strcmp('50', '125')."\n");                      // '50' vs '125' = 4
+echo PHP_EOL.'Using strcmp function - case-sensitive:'.PHP_EOL;
+print_r(strcmp("hello", 'hello')    .PHP_EOL);  // -32
+print_r(strcmp('hello',"hello")     .PHP_EOL);  // 0
+print_r(strcmp('Portugal','Lisbon') .PHP_EOL);  // 4
+print_r(strcmp('Lisbon','Portugal') .PHP_EOL);  // -4
+print_r(strcmp('Hello', "hello")    .PHP_EOL);  // -32
+print_r(strcmp('Hello', 'hello')    .PHP_EOL);  // -32
+print_r(strcmp('hello', 'Hello')    .PHP_EOL);  // 32
+print_r(strcmp('1', '0125')         .PHP_EOL);  // 1
+print_r(strcmp('1', 1)              .PHP_EOL);  // 0
+print_r(strcmp('1', 01)             .PHP_EOL);  // 0
+print_r(strcmp('01', 01)            .PHP_EOL);  // -1
+print_r(strcmp('1', '01')           .PHP_EOL);  // 1
+print_r(strcmp('125', '25')         .PHP_EOL);  // -1
+print_r(strcmp('50', '125')         .PHP_EOL);  // 4
 
 // strcmp function - case-insensitive
 echo PHP_EOL.'Using strcmp function - case-insensitive'.PHP_EOL;
